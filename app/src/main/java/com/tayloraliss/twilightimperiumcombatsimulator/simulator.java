22b6c40fa892e809b2f80e055bfd2357df41c96e
Taylor Liss <tayloraliss@gmail.com>
2017-03-28 23:23:54 -0400

    Add variables to simulator activity

--- app/src/main/java/com/tayloraliss/twilightimperiumcombatsimulator/simulator.java
+++ app/src/main/java/com/tayloraliss/twilightimperiumcombatsimulator/simulator.java
@@ -5,9 +5,46 @@ import android.os.Bundle;
 
 public class simulator extends AppCompatActivity {
 
+    int yourFleetNumberOfFighters;
+    int yourFleetNumberOfCarriers;
+    int yourFleetNumberOfDestroyers;
+    int yourFleetNumberOfCruisers;
+    int yourFleetNumberOfDreadnaughts;
+    int yourFleetNumberOfWarsuns;
+
+    int enemyFleetNumberOfFighters;
+    int enemyFleetNumberOfCarriers;
+    int enemyFleetNumberOfDestroyers;
+    int enemyFleetNumberOfCruisers;
+    int enemyFleetNumberOfDreadnaughts;
+    int enemyFleetNumberOfWarsuns;
+
+    @Override
+    public void onBackPressed() {
+        this.finish();
+        overridePendingTransition(0, 0);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simulator);
+        getSupportActionBar().hide();
+
+        Bundle myBundle = getIntent().getExtras();
+
+        yourFleetNumberOfFighters = myBundle.getInt("yourFighters");
+        yourFleetNumberOfCarriers = myBundle.getInt("yourCarriers");
+        yourFleetNumberOfDestroyers = myBundle.getInt("yourDestroyers");
+        yourFleetNumberOfCruisers = myBundle.getInt("yourCruisers");
+        yourFleetNumberOfDreadnaughts = myBundle.getInt("yourDreadnaughts");
+        yourFleetNumberOfWarsuns = myBundle.getInt("yourWarsuns");
+
+        enemyFleetNumberOfFighters = myBundle.getInt("enemyFighters");
+        enemyFleetNumberOfCarriers = myBundle.getInt("enemyCarriers");
+        enemyFleetNumberOfDestroyers = myBundle.getInt("enemyDestroyers");
+        enemyFleetNumberOfCruisers = myBundle.getInt("enemyCruisers");
+        enemyFleetNumberOfDreadnaughts = myBundle.getInt("enemyDreadnaughts");
+        enemyFleetNumberOfWarsuns = myBundle.getInt("enemyWarsuns");
     }
 }
